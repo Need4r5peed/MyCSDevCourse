45d8f2d4626e9d4bda9ad50c59b58e0ec89cafc5
Need4r5peed <windrayn@yandex.ru>
2025-04-29 18:18:48 +0300

    Реализация неявного интерфейса (без вызова) Задание 10.2.3 Реализация интерфейсов

---
+++ Module/Module10/Unit2/Module10.Unit2.Task3/MyWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10.Unit2.Task3
+{
+    class MyWorker : IWorker
+    {
+        public void Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

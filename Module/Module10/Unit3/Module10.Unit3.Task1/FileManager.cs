ff62d1a5e034b73b6a835d755ec0b6034e696c97
Need4r5peed <windrayn@yandex.ru>
2025-04-29 20:34:18 +0300

    Исполнение в классе FileManager множественной реализации интерфесов. Задание 10.3.1 Множ. реал. интерфесов

---
+++ Module/Module10/Unit3/Module10.Unit3.Task1/FileManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10.Unit3.Task1
+{
+    class FileManager : 
+        IWriter, 
+        IReader, 
+        IMailer
+    {
+        void IReader.Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IMailer.SendEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IWriter.Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

3d993266e59d043c2a5f61316609a0661ecec7c3
Need4r5peed <windrayn@yandex.ru>
2025-04-29 18:28:03 +0300

    Явная реализаци (того же) интерфейса с вызовом Задание 10.2.4 Реализация интерфесов

---
+++ Module/Module10/Unit2/Module10.Unit2.Task3/MyWorker2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10.Unit2.Task3
+{
+    class MyWorker2
+    {
+    }
+}

7237b560fcefaf363f32fb7dd74030caecadedf9
Need4r5peed <windrayn@yandex.ru>
2025-08-21 22:11:17 +0300

    Создан проект Module11.Unit2.UtilityBot

--- Module/Module11/Unit2/Module11.Unit2.VoiceTexterBot/Program.cs
+++ Module/Module11/Unit2/Module11.Unit2.VoiceTexterBot/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Text;
+using Telegram.Bot;
+
+namespace Module11.Unit2.VoiceTexterBot
+{
+    public class Main_VoiceTexterBot
+    {
+
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("7947826604:AAE0gKiyi5rWEKFCkh7qfrBGX6zGzja4mhM"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+    }
+}
